af6284158a090b038e3462a0e1fd6e5a6d0293ab
YPUN <guo88882@gmail.com>
2019-12-18 14:46:35 +0800

    edit5

--- scripts/index/renderer.js
+++ scripts/index/renderer.js
@@ -140,16 +140,16 @@ function vmStart() {
                     console.log('update_available')
                     ipcRenderer.removeAllListeners('update_available');
                     document.getElementById('message').innerText = 'A new update is available. Downloading now...';
-                    document.getElementById('notification').classList.remove('hidden');
-                    document.getElementById('restartButton').classList.remove('hidden');
+                  //  document.getElementById('notification').classList.remove('hidden');
+                  //  document.getElementById('restartButton').classList.remove('hidden');
 
                 });
                 ipcRenderer.on('update_downloaded', () => {
                     console.log('update_downloaded')
                     ipcRenderer.removeAllListeners('update_downloaded');
                     document.getElementById('message').innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
-                    document.getElementById('restartButton').classList.remove('hidden');
-                    document.getElementById('notification').classList.remove('hidden');
+                    //document.getElementById('restartButton').classList.remove('hidden');
+                   // document.getElementById('notification').classList.remove('hidden');
                 });
 
             });
